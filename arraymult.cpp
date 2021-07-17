80ed384aba5cd741822b80df823b722b926dc582
Joshua Laughner <jllacct119@gmail.com>
2021-07-16 21:24:30 -0700

    Replaced C++ file with one that reallocates every loop

--- arraymult.cpp
+++ arraymult.cpp
@@ -14,27 +14,36 @@ void array_mult(float* a, float* b, float* c, int n){
     }
 }
 
-
-int main() {
+void array_test(int j) {
     int n = 100000000;
     float* a = (float*)malloc(n*sizeof(float));
     float* b = (float*)malloc(n*sizeof(float));
     auto t1 = high_resolution_clock::now();
     float* c = (float*)malloc(n*sizeof(float));
     auto t2 = high_resolution_clock::now();
-    auto dur = duration_cast<microseconds>(t2-t1);
-    std::cout << "Allocation time = " << dur.count() << " us" << std::endl;
+    auto dur12 = duration_cast<microseconds>(t2-t1);
+    std::cout << "   * Allocation time = " << dur12.count() << " us" << std::endl;
 
     for(int i=0; i<n; ++i){
-        a[i] = i;
-        b[i] = 2*i;
+        a[i] = i+j;
+        b[i] = 2*i+j;
     }
 
+    auto t3 = high_resolution_clock::now();
+    array_mult(a,b,c,n);
+    auto t4 = high_resolution_clock::now();
+    auto dur34 = duration_cast<milliseconds>(t4 - t3);
+    std::cout << "   * Multiplication time = " << dur34.count() << " ms" << std::endl;
+
+    free(a);
+    free(b);
+    free(c);
+}
+
+
+int main() {
     for(int j=0; j<5; ++j){
-        auto t1 = high_resolution_clock::now();
-        array_mult(a,b,c,n);
-        auto t2 = high_resolution_clock::now();
-        auto dur = duration_cast<milliseconds>(t2 - t1);
-        std::cout << "Time to multiply in loop " << j << " time = " << dur.count() << " ms" << std::endl;
+        std::cout << " Loop " << j+1 << "/5" << std::endl;
+        array_test(j);
     }
 }
